1afb6effc8571b07dea9232df5458273c1735e29
narek-voskanyan <narek.voskanyan91@gamil.com>
2022-03-14 22:32:50 +0400

    Initial commit

---
+++ src/ArrayListVsLinkedListTesting/Main.java
@@ -0,0 +1,60 @@
+package ArrayListVsLinkedListTesting;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        Main testOne = new Main();
+
+
+
+        LinkedList<Students> list1 = testOne.createMilionLinkedList();
+        ArrayList<Students> list2 = testOne.createMilionArrayList();
+
+        long s1Start = System.currentTimeMillis();
+        Students person = list1.get(500000);
+        long s1End = System.currentTimeMillis();
+
+        System.out.println(person.getName() + ": " + person.getAge());
+
+
+
+
+        long s2Start = System.currentTimeMillis();
+        Students people = list2.get(500000);
+        long s2End = System.currentTimeMillis();
+
+        System.out.println(people.getName() + ": " + people.getAge());
+
+        System.out.println("LinkedList: " + (s1End - s1Start) + " " + "ArrayList: " + (s2End - s2Start));
+
+    }
+
+
+    //EXAMPLE BY LINKEDLIST
+    public LinkedList<Students> createMilionLinkedList(){
+
+        LinkedList<Students> exampleByList = new LinkedList<Students>();
+
+        for(int i = 0; i < 1000000; i++){
+            exampleByList.addFirst(new Students());
+        }
+        return exampleByList;
+    }
+
+
+    //EXAMPLE BY ARRAYLIST
+    public ArrayList<Students> createMilionArrayList(){
+
+        ArrayList<Students> exampleByArray = new ArrayList<Students>(1000000);
+
+        for(int i = 0; i < 1000000; i++){
+            exampleByArray.add(new Students());
+        }
+        return  exampleByArray;
+    }
+}
---
+++ src/ArrayListVsLinkedListTesting/Students.java
@@ -0,0 +1,41 @@
+package ArrayListVsLinkedListTesting;
+
+public class Students {
+    Students(){
+
+        getAge();
+        getName();
+    }
+
+    private String name;
+    private int age;
+
+    public int getAge(){
+        int age = 18;
+        this.age = age + (int)(Math.random() * 7);
+        return this.age;
+    }
+
+    public String getName(){
+        String name = "";
+        String allLetters = "abcdefghijklmnopqrstuvwxyz";
+        int nameLength = 3;
+        int forLetter = 0;
+
+        nameLength += (int)(Math.random() * 8);
+
+        for(int i = 0; i < nameLength; i++ ){
+
+            forLetter = (int)(Math.random() * allLetters.length());
+            name += allLetters.charAt(forLetter);
+
+        }
+
+        this.name = name.substring(0,1).toUpperCase() + name.substring(1);
+        return this.name;
+    }
+
+    public void setInformation(){
+        System.out.println(getName() + " " + getAge() );
+    }
+}
